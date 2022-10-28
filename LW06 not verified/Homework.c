4acd7b92c6b64de5d373858ae4e9e5c674a76892
rek655689 <e.gladneva1357@gmail.com>
2022-10-28 20:52:37 +0300

    right if-else

--- LW06 not verified/Homework.c
+++ LW06 not verified/Homework.c
@@ -3,8 +3,6 @@
 #include <stdio.h>
 #include <math.h>
 
-// наверное надо будет ещё на функции разбивать
-
 /* ЗАДАЧА 1
 	#define a 3 
 	#define b 5
@@ -37,9 +35,13 @@ void main(void)
 int checking(double x, double y)
 {
 	if (x > 0 && y > 0) return 1;
-	else if (x < 0 && y > 0) return 2;
-	else if (x < 0 && y < 0) return 3;
-	else return 4;
+	else {
+		if (x < 0 && y > 0) return 2;
+		else {
+			if (x < 0 && y < 0) return 3;
+			else return 4;
+		}
+	}
 };
 
 void main(void)
