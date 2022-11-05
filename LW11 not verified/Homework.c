2aee8cfc11c7033d7f2fba24ef9bad4e3b1d432a
rek655689 <e.gladneva1357@gmail.com>
2022-11-05 13:43:43 +0300

    add lw

---
+++ LW11 not verified/Homework.c
@@ -0,0 +1,31 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <locale.h>
+#include <math.h>
+#define N 10
+
+/* С одномерным массивом из n вводимых положительных и отрицательных целых чисел высчитать:
+	3. Сумму всех элементов массива, расположенных до последнего положительного элемента. */
+
+void main()
+{
+	setlocale(LC_ALL, "RUS");
+	
+    int A[N];
+    int x;
+    int sum = 0, possible_sum = 0;
+    for (int i = 0; i < N; i++)
+    {
+        printf("A[%d] = ", i);
+        scanf("%d", &x);
+        A[i] = x;
+
+
+        if (x > 0) {
+            sum += possible_sum;
+            possible_sum = 0;
+        }
+        possible_sum += x;
+    }
+    printf("Сумма элементов до последнего положительного = %d", sum);
+}
