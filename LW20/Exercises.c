920c5953fdf9ae2bbe7108560eb0a71914372605
rek655689 <e.gladneva1357@gmail.com>
2022-12-22 23:33:54 +0300

    add LW

---
+++ LW20/Exercises.c
@@ -0,0 +1,64 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <string.h>
+#define N 30
+
+void main1()
+{
+	setlocale(LC_ALL, "RUS");
+
+	// ЗАДАНИЕ 1. Напишите программу, которая вычисляет среднее арифметическое целых чисел, содержащихся в файле
+	FILE* file;
+	float a, sum = 0, n = 0, av;
+	if ((file = fopen("temp.txt", "rt")) == NULL)
+	{
+		printf("Ошибка открытия файла");
+		exit(-1);
+	}
+	while (!feof(file))
+	{
+		fscanf(file, "%f", &a);
+		sum += a;
+		n++;
+	}
+	av = (float)sum / n;
+	printf("Среднее арифметическое: %g\n\n", av);
+	fclose(file);
+
+	// ЗАДАНИЕ 2. Программа, которая открывает указанный пользователем файл, выполняет преобразование над числами, записывает результат в файл с дополненным названием
+	//			  3. Уменьшить все положительные числа в 4 раза.
+	FILE *new_file;
+	char name[N+4], new_name[N+12];
+	printf("Введите название текстового файла (без расширения):\n>");
+	fgets(name, N, stdin);
+	for (int i = 0; i < N - 1; i++)
+	{
+		if (name[i] == '\n')
+			name[i] = name[i + 1];
+	}
+	strcpy(new_name, name);
+	strcat(name, ".txt");
+	if ((file = fopen(name, "rt")) == NULL)
+	{
+		printf("Ошибка открытия файла %s", name);
+		exit(-1);
+	}
+	strcat(new_name, "_reduce.txt");
+	if ((new_file = fopen(new_name, "wt")) == NULL)
+	{
+		printf("Ошибка открытия файла %s", name);
+		exit(-1);
+	}
+	while (!feof(file))
+	{
+		fscanf(file, "%f", &a);
+		a = a > 0 ? a / 4 : a;
+		fprintf(new_file, "%f\n", a);
+	}
+	fclose(file);
+	fclose(new_file);
+	printf("Задача 2 завершена");
+
+}
---
+++ LW20/Homework.c
@@ -0,0 +1,53 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <string.h>
+#define N 30
+
+/* Считать текстовый файл winter.txt и выполнить над текстом преобразование, полученный вариант текст записать
+ вновый файл с именем, указанным пользователем
+	3. Заменить пробелами все буквы о, а буквы o на пробелы */
+
+void main()
+{
+	setlocale(LC_ALL, "RUS");
+	FILE* file, * new_file;
+	char name[N+4], str[N];
+	printf("Укажите название нового текстового файла (без расширения):\n>");
+	fgets(name, N, stdin);
+	for (int i = 0; i < N - 1; i++)
+	{
+		if (name[i] == '\n')
+			name[i] = name[i + 1];
+	}
+	strcat(name, ".txt");
+	if ((file = fopen("winter.txt", "rt")) == NULL)
+	{
+		printf("Ошибка открытия файла winter.txt");
+		exit(-1);
+	}
+	if ((new_file = fopen(name, "wt")) == NULL)
+	{
+		printf("Ошибка открытия файла %s", name);
+		exit(-1);
+	}
+	while (!feof(file))
+	{
+		fgets(str, N, file);
+		for (int i = 0; i < N; i++)
+		{
+			if (str[i] == 'o')
+			{
+				str[i] = ' ';
+				continue;
+			}
+			if (str[i] == ' ')
+				str[i] = 'o';
+		}
+		fprintf(new_file, "%s", str);
+	}
+	fclose(file);
+	fclose(new_file);
+	printf("Программа завершена");
+}
